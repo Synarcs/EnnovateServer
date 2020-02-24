b4393d6c475b0bb2fd5c700ad6cab3972be84379
vedangparasnis <vedang.parasnis921@gmail.com>
2020-02-24 23:51:38 +0530

    configs

---
+++ configs/config.js
@@ -0,0 +1,40 @@
+module.exports.firebaseConfig = {
+  apiKey: "AIzaSyAPdZz1Uz4zh3iIJBc_Av3_Zhpa7imLmCQ",
+  authDomain: "registration-details-a127d.firebaseapp.com",
+  databaseURL: "https://registration-details-a127d.firebaseio.com",
+  projectId: "registration-details-a127d",
+  storageBucket: "registration-details-a127d.appspot.com",
+  messagingSenderId: "274203592486",
+  appId: "1:274203592486:web:7f4afd24615dad2b"
+};
+
+module.exports.maliguns = {
+  DOMAIN: "sandboxa4fce1c5178d4ca085284b46fa3cc935.mailgun.org",
+  apiKey: "e6c6bee0f0bfffe220e983e91a3d99f1-52b6835e-729b8624"
+};
+
+// facebook
+module.exports.facebook = {
+  clientID: 647495802726332,
+  clientSecret: "20f9a2ab75beecc5cad3e06e35e3e691"
+};
+
+// github
+module.exports.github = {
+  githubclientID: "5a1e7b628be9de660955",
+  githubclientSecret: "461c5c0b245590fdc8c9789d1a86505fecb24cfb"
+};
+
+module.exports.google = {
+  googleId:
+    "664011953624-l2ekqfniaph07qfmcc1to2e0ngj65ubu.apps.googleusercontent.com",
+  googleSecret: "FUvp8SPN8txe2Y2aHyXsWKrh"
+};
+
+// app level
+module.exports.developmentUrl =
+  "http://localhost:5001/registration-details-a127d/us-central1/Ennovate";
+
+const data = "d&3q*p+u]trG=Aj$6D";
+
+// development everyone can access cloud functions
