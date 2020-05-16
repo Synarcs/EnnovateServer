94c9af014ce392217fc16d1c87b49c50869e47db
vedangparasnis <35874709+vedangparasnis@users.noreply.github.com>
2020-05-17 00:00:43 +0530

    config-changes

--- configs/config.js
+++ configs/config.js
@@ -1,34 +1,34 @@
 module.exports.firebaseConfig = {
-  apiKey: "AIzaSyAPdZz1Uz4zh3iIJBc_Av3_Zhpa7imLmCQ",
-  authDomain: "registration-details-a127d.firebaseapp.com",
-  databaseURL: "https://registration-details-a127d.firebaseio.com",
-  projectId: "registration-details-a127d",
-  storageBucket: "registration-details-a127d.appspot.com",
-  messagingSenderId: "274203592486",
-  appId: "1:274203592486:web:7f4afd24615dad2b"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 module.exports.maliguns = {
-  DOMAIN: "sandboxa4fce1c5178d4ca085284b46fa3cc935.mailgun.org",
-  apiKey: "e6c6bee0f0bfffe220e983e91a3d99f1-52b6835e-729b8624"
+  DOMAIN: "",
+  apiKey: ""
 };
 
 // facebook
 module.exports.facebook = {
-  clientID: 647495802726332,
-  clientSecret: "20f9a2ab75beecc5cad3e06e35e3e691"
+  clientID: ,
+  clientSecret: ""
 };
 
 // github
 module.exports.github = {
-  githubclientID: "5a1e7b628be9de660955",
-  githubclientSecret: "461c5c0b245590fdc8c9789d1a86505fecb24cfb"
+  githubclientID: "",
+  githubclientSecret: ""
 };
 
 module.exports.google = {
   googleId:
-    "664011953624-l2ekqfniaph07qfmcc1to2e0ngj65ubu.apps.googleusercontent.com",
-  googleSecret: "FUvp8SPN8txe2Y2aHyXsWKrh"
+    "",
+  googleSecret: ""
 };
 
 // app level
